5ff62e360445d2966a13bb8cef8df22596deb951
Vaness <vaness13337@gmail.com>
2022-07-27 16:50:07 +0700

    Adding jump to player script

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,6 +7,9 @@ public class PlayerMovement : MonoBehaviour
     Transform trans_component;
     Animator animator;
     public Vector2 prev_pos;
+    Rigidbody2D rigidbody;
+    BoxCollider2D BoxCollider;
+    public bool isjumping;
 
     // Start is called before the first frame update
     void Start()
@@ -15,20 +18,35 @@ public class PlayerMovement : MonoBehaviour
         animator = GetComponent<Animator>();
         animator.speed = 0.5f;
         prev_pos = trans_component.position;
+        rigidbody = GetComponent<Rigidbody2D>();
+        BoxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private bool grounded()
+    {
+        float extraspace = 0.1f;
+        RaycastHit2D hit = Physics2D.Raycast(BoxCollider.bounds.center, Vector2.down, BoxCollider.bounds.extents.y + extraspace );
+        Debug.Log(hit.distance);
+        Debug.Log(hit.collider);
+        Debug.DrawRay(BoxCollider.bounds.center, (Vector2.down * BoxCollider.bounds.extents), Color.green, 0.05f);
+        if(hit.collider == null ){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButton("Horizontal"))
+        prev_pos = trans_component.position;
+        if (Input.GetButton("Horizontal"))
         {
             float input = Input.GetAxis("Horizontal");
-            Vector2 new_val = new Vector2(input * Time.deltaTime * 3 + prev_pos.x, 0);
+            Vector2 new_val = new Vector2(input * Time.deltaTime * 3 + prev_pos.x, prev_pos.y);
             trans_component.position = new_val;
 
             animator.Play("Running");
-
-            Debug.Log(new_val.x - prev_pos.x);
             if (new_val.x - prev_pos.x <= 0f)
             {
                 trans_component.rotation = Quaternion.Euler(0, 180, 0);
@@ -38,9 +56,20 @@ public class PlayerMovement : MonoBehaviour
                 trans_component.rotation = Quaternion.Euler(0, 0, 0);
             }
 
-            prev_pos = new_val;
-        } else {
+            /*prev_pos = new_val;*/
+        }
+        else
+        {
             animator.Play("Idle");
+        }
+
+
+        if (Input.GetButtonDown("Jump") && !isjumping) {
+            isjumping = true;
+            rigidbody.AddForce(new Vector2(0, 300f));
+        } else if (!Input.GetButtonDown("Jump"))
+        {
+            isjumping = grounded();
         }
     }
 
